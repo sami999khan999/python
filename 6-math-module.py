d617666a07b82bdb9ed468081a613790fbb2874b
Sami Khan <sami999khan999@gmail.com>
2024-08-12 14:27:43 +0600

    miner changes

--- 6-math-module.py
+++ 6-math-module.py
@@ -6,6 +6,8 @@ pi = 3.1416
 
 # ===================================================================================================== #
 
+# round 
+
 # The round() function in Python is used to round a floating-point number to a specified number of decimal places. If no number of decimal places is specified, it rounds to the nearest integer.
 
 print(round(pi))
@@ -13,11 +15,15 @@ print(round(pi, 2))
 
 # ===================================================================================================== #
 
+# ceil
+
 # The math.ceil() function returns the smallest integer greater than or equal to a given number. It is useful for rounding up numbers to the nearest whole number.
 
 print(math.ceil(pi))
 
 # ===================================================================================================== #
+
+# floor
 
 # The math.floor() function returns the largest integer less than or equal to a given number. It is useful for rounding down numbers to the nearest whole number.
 
@@ -25,11 +31,15 @@ print(math.floor(pi))
 
 # ===================================================================================================== #
 
+# abs
+
 # The abs() function in Python returns the absolute value of a number. The absolute value is the non-negative value of the number, which means it strips away any negative sign.
 
 print(abs(pi))
 
 # ===================================================================================================== #
+
+# pow
 
 # The pow() function in Python is used to raise a number to a specified power or to calculate the result of a number raised to a power with an optional modulus.
 
@@ -37,11 +47,15 @@ print(pow(pi,2))
 
 # ===================================================================================================== #
 
+# sqrt
+
 # The sqrt() function in Python computes the square root of a number. It is part of the math module.
 
 print(math.sqrt(pi))
 
 # ===================================================================================================== #
+
+# max and min
 
 # The max() and min() functions in Python are used to find the largest and smallest values, respectively, from a set of values or within a sequence like a list or tuple.
 
